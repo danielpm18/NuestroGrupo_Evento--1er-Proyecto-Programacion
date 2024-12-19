bf9830fe8da83d8f1937bfb09cc6c906fa147739
Bntt20 <eddybrent10@gmail.com>
2024-12-19 17:44:39 +0100

    Avances del Main 2.0 Y agregar funciones mas Eventos

--- proyecto/src/Evento.java
+++ proyecto/src/Evento.java
@@ -1,8 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Evento {
+    private static final String ARCHIVO = "eventos.txt";
+    private static final Evento[] evento = new Evento[150];
+    private static int contador = 0;
+    Scanner scanner = new Scanner(System.in);
 
     //VARIABLES DEL EVENTO
     public int id;
     public String nombre;
+    public String fecha;
     public String horaInicio;
     public String horaFin;
     public String lugar;
@@ -11,10 +20,11 @@ public class Evento {
 
 
     //CONSTRUCTOR DE EVENTOS
-    public Evento(int id, String nombre, String horaInicio, String horaFin, String lugar, int publicoObjetivo, String urlRegistro) {
+    public Evento(int id, String nombre,String fecha, String horaInicio, String horaFin, String lugar, int publicoObjetivo, String urlRegistro) {
 
         this.id = id;
         this.nombre = nombre;
+        this.fecha = fecha;
         this.horaInicio = horaInicio;
         this.horaFin = horaFin;
         this.lugar = lugar;
@@ -23,9 +33,21 @@ public class Evento {
 
     }
 
-    public String escribirEvento() {
-        return id+";"+nombre+";"+horaInicio+";"+horaFin+";"+lugar+";"+publicoObjetivo+";"+urlRegistro;
+    //METODO PARA CARGAR EL EVENTO EN CONSOLA
+    public static void cargarEvento() {
+        try(BufferedReader br = new BufferedReader(new FileReader("eventos.txt"))){
+            String linea;
+            while((linea = br.readLine()) != null){
+                evento[contador++] = Evento.fromString(Linea);
+            }
+        }catch (Exception e){
+            System.out.println("No se pudo cargar el archivo: " + e.getMessage());
+        }
 
+    }
+    //METODO PARA CREAR EL EVENTO EN CONSOLA
+    public String escribirEvento() {
+        return id+";"+fecha+";"+nombre+";"+horaInicio+";"+horaFin+";"+lugar+";"+publicoObjetivo+";"+urlRegistro;
     }
 
     //METODO PARA MOSTRAR EL EVENTO EN CONSOLA
@@ -33,11 +55,34 @@ public class Evento {
 
         System.out.println("ID: " + id);
         System.out.println("NOMBRE: " + nombre);
+        System.out.println("FECHA: " + fecha);
         System.out.println("HORA INICIO: " + horaInicio);
         System.out.println("HORA FIN: " + horaFin);
         System.out.println("LUGAR: "+ lugar);
         System.out.println("PUBLICO: " + (publicoObjetivo==1 ? "Estudiantes" : (publicoObjetivo==2 ? "Profesres" : "Tod@s en la Universidad")));
         System.out.println("URL: " + urlRegistro);
     }
+    //METODO PARA ACTUALIZAR EL EVENTO EN CONSOLA
+    public void actualizarEvento() {
+        System.out.println("Ingrese el ID del evento a actualizar:");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Ingrese el nombre del evento a actualizar:");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese la fecha del evento a actualizar:");
+        String fecha = scanner.nextLine();
+        System.out.println("Ingrese la hora de inicio del evento a actualizar:");
+        String horaInicio = scanner.nextLine();
+        System.out.println("Ingrese la hora de fin del evento a actualizar:");
+        String horaFin = scanner.nextLine();
+        System.out.println("Ingrese el lugar del evento a actualizar:");
+        String lugar = scanner.nextLine();
+        System.out.println("Ingrese el objetivo del evento a actualizar:");
+        int publicoObjetivo = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Ingrese la URL del evento a actualizar:");
+        String urlRegistro = scanner.nextLine();
+    }
+
 
 }
--- proyecto/src/Main.java
+++ proyecto/src/Main.java
@@ -1,11 +1,33 @@
 import java.util.Scanner;
 import java.io.File;
 public class Main {
-    public static void main(String[] args){
-        Evento [] evento = new Evento[150];
-        System.out.print("ID:");
+    private static final String ARCHIVO = "eventos.txt";
+    private static Evento[] evento = new Evento[150];
+    private static int contador = 0;
 
-        System.out.println("Que es lo que deseas realizar? (1: Registrar Evento \n 2: Mostrar Eventos \n 3: Quitar Evento \n 4:Salir)");
+    public static void main(String[] args) {
+        //cargareventos();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n-Bienvenido a nuestro calendario de Eventos-\n");
+            System.out.println("¿Que es lo que deseas realizar? \n 1: Registrar Evento \n 2: Mostrar Eventos \n 3: Quitar Evento \n 4:Salir ");
+            int opcion = scanner.nextInt();
+            scanner.nextLine();
 
+            switch (opcion) {
+                case 1:
+                    registrarEvento(scanner);
+                    break;
+                case 2:
+                    mostrarevento(scanner);
+                    break;
+                case 3:
+                    ...
+
+            }
+
+        }
     }
-}
+        }
+
+
