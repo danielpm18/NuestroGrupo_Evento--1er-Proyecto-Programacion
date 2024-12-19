a1ae5f1e5c1f99fd0332e98be078269395f1a53e
Bntt20 <eddybrent10@gmail.com>
2024-12-19 13:46:45 +0100

    Avance del Main

--- proyecto/src/Main.java
+++ proyecto/src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+import java.io.File;
 public class Main {
     public static void main(String[] args){
+        Evento [] evento = new Evento[150];
+        System.out.print("ID:");
 
-
+        System.out.println("Que es lo que deseas realizar? (1: Registrar Evento \n 2: Mostrar Eventos \n 3: Quitar Evento \n 4:Salir)");
 
     }
 }
